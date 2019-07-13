4d94f14f22541456c9505ba6c8acdfd78da43b97
mike_neck <jkrt3333@gmail.com>
2019-07-13 15:37:36 +0900

    add panel number

--- src/game.tsx
+++ src/game.tsx
@@ -1,36 +1,38 @@
 import * as React from "react";
-import * as styles from "./index.css";
+import "./index.css";
 
-class Square extends React.Component<{}, {}> {
+interface Tile {
+  position: number;
+}
+
+class Square extends React.Component<Tile, {}> {
   render() {
     return (
-      <button className={ styles.square }>
-        { /* TODO */ }
-      </button>
+      <button className="square">{ this.props.position }</button>
     );
   }
 }
 
 class Board extends React.Component<{}, {}> {
   renderSquare(position: number) {
-    return <Square />;
+    return <Square position={ position } />;
   }
   render() {
     let status = "Next player: X";
     return (
       <div>
-        <div className={ styles.status }>{ status }</div>
-        <div className={ styles["board-row"] }>
+        <div className="status">{ status }</div>
+        <div className="board-row">
           { this.renderSquare(0) }
           { this.renderSquare(1) }
           { this.renderSquare(2) }
         </div>
-        <div className={ styles["board-row"] }>
+        <div className="board-row">
           { this.renderSquare(3) }
           { this.renderSquare(4) }
           { this.renderSquare(5) }
         </div>
-        <div className={ styles["board-row"] }>
+        <div className="board-row">
           { this.renderSquare(6) }
           { this.renderSquare(7) }
           { this.renderSquare(8) }
@@ -43,11 +45,11 @@ class Board extends React.Component<{}, {}> {
 export class Game extends React.Component<{}, {}> {
   render() {
     return (
-      <div className={ styles.game }>
+      <div className="game">
         <div className="game-board">
           <Board />
         </div>
-        <div className={ styles["game-info"] }>
+        <div className="game-info">
           <div>{ /* status */ }</div>
           <ol>{ /* todo */ }</ol>
         </div>
